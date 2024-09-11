b614d2681abdaa37bf961aea20ecd96ca3a54f4a
KarolynneFreire <karolynnefreire@gmail.com>
2024-09-10 23:25:23 -0300

    incluindo o footer

--- front/projetostartia/src/App.jsx
+++ front/projetostartia/src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './assets/components/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/* O componente Home será renderizado pela rota, então remova-o daqui */}
       <Outlet /> {/* Isso renderiza as rotas filhas (Home, Cadastro, Consulta, etc.) */}
       {/* <ChatBot flow={flow} /> */}
+      <Footer></Footer>
     </div>
 
   );
---
+++ front/projetostartia/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css'; // Importa o arquivo de estilos CSS
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+        </div>
+        <div className="copyright">
+          <p>© 2023 Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
