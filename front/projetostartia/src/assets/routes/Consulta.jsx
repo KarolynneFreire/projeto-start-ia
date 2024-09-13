d0f0f42ee9b2d0de207d0e59f31476ffac8f7fd8
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2024-09-12 23:23:25 -0300

    adiciona paginacao infinita

--- front/projetostartia/src/assets/routes/Consulta.jsx
+++ front/projetostartia/src/assets/routes/Consulta.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-
-import '../css/Consulta.css'
+import '../css/Consulta.css';
 
 function Consulta() {
-
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1); // Página inicial
+  const [hasMore, setHasMore] = useState(true); // Indicador de mais dados
+
+  const fetchPosts = useCallback(async () => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/v1/api/usuarios/?skip=${(page - 1) * 100}&limit=100`);
+      setPosts(prevPosts => [...prevPosts, ...response.data]);
+      setHasMore(response.data.length > 0);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [page]);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/v1/api/usuarios/?skip=0&limit=100')
+    fetchPosts();
+  }, [fetchPosts]);
 
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || !hasMore) return;
+      setPage(prevPage => prevPage + 1);
+    };
 
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [hasMore]);
 
   return (
     <div>
-      {/* <div className='consulta-pesquisa'>
-        <input type="search" id='nome' className='input-pesquisa' />
-        <input type="search" name="" id="" className='input-pesquisa' />
-        <input type="search" name="" id="" className='input-pesquisa' />
-        <input type="search" name="" id="" className='input-pesquisa' />
-        <input type="search" name="" id="" className='input-pesquisa' />
-      </div> */}
       <div className="user-container">
         {posts.map(post => (
           <button key={post.id} className='btn-pessoa'>
@@ -40,6 +44,7 @@ function Consulta() {
             </p>
           </button>
         ))}
+        {!hasMore && <p>Não há mais usuários para carregar.</p>}
       </div>
     </div>
   );
