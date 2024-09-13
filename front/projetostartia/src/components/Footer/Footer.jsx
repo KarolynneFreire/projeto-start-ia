c41e9daa0553f0998a32d1f2f38199894b0090ce
José Edberto Santos de França <edbertosantos.dev@gmail.com>
2024-09-13 05:13:50 -0300

    Ajeitando quebra da tela de cadastro

--- front/projetostartia/src/components/Footer/Footer.jsx
+++ front/projetostartia/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="container-footer">
         <div className="row">
         </div>
         <div className="copyright">
